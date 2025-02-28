4581748867a2455e5590a2a053bd376d694fdb1f
hadi alghoul <hadialghoul2003@gmail.com>
2025-02-28 11:46:40 +0200

    4 commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME':  BASE_DIR / os.getenv('DB_NAME', 'db.sqlite3'),
     }
 }
 
