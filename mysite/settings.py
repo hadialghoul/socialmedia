bd10368184e5d37ec1d51a741eb586af656ac71c
hadi alghoul <hadialghoul2003@gmail.com>
2025-02-28 11:07:27 +0200

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['socialmedia-phi-three.vercel.app','*']
 
 
 # Application definition
